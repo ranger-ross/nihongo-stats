3ab92e06eaba4f5eec26640cbb19e7f85a92714a
Ross Sullivan <sullivan.ross@heb.com>
2021-11-07 09:36:22 -0600

    Added get subjects for wanikani

--- src/wanikani/WanikaniDashboard.jsx
+++ src/wanikani/WanikaniDashboard.jsx
@@ -2,8 +2,8 @@ import makeStyles from "@material-ui/core/styles/makeStyles";
 import { useWanikaniApiKey } from "./stores/WanikaniApiKeyStore";
 import { Navigate } from "react-router";
 import { RoutePaths } from "../Routes";
-import WanikaniLevelProgessChart from "./charts/WanikaniLevelProgressChart";
 import { Card } from "@material-ui/core";
+import WanikaniApiService from "./service/WanikaniApiService";
 
 const useStyles = makeStyles({
     container: {}
--- src/wanikani/service/WanikaniApiService.js
+++ src/wanikani/service/WanikaniApiService.js
@@ -1,12 +1,10 @@
 const wanikaniApiUrl = 'https://api.wanikani.com'
 
-const authHeader = (apiKey) => ({'Authorization': `Bearer ${apiKey}`})
+const authHeader = (apiKey) => ({ 'Authorization': `Bearer ${apiKey}` })
 
 function fetchWanikaniApi(path, apiKey) {
     return fetch(`${wanikaniApiUrl}${path}`, {
-        headers: {
-            ...authHeader(apiKey)
-        }
+        headers: { ...authHeader(apiKey) }
     })
 }
 
@@ -14,4 +12,22 @@ export default {
     getUser: (apiKey) => fetchWanikaniApi('/v2/user', apiKey),
     getReviews: (apiKey) => fetchWanikaniApi('/v2/reviews', apiKey),
     getLevelProgress: (apiKey) => fetchWanikaniApi('/v2/level_progressions', apiKey),
+    getSubjects: async (apiKey) => {
+        const firstPage = await (await fetch(`${wanikaniApiUrl}/v2/subjects`, {
+            headers: { ...authHeader(apiKey) },
+            cache: 'force-cache'
+        })).json()
+        let data = firstPage.data;
+        let nextPage = firstPage.pages['next_url']
+
+        while (!!nextPage) {
+            const page = await (await fetch(nextPage, {
+                headers: authHeader(apiKey),
+                cache: 'force-cache'
+            })).json();
+            data = data.concat(page.data);
+            nextPage = page.pages['next_url'];
+        }
+        return data;
+    },
 }

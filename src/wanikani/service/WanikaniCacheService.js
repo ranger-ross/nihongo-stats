f760d129bb13ebacbcc3ccea10844f0e2e86aa28
Ross Sullivan <rosssullivan101@gmail.com>
2021-11-21 10:02:23 -0600

    clean up

--- src/wanikani/service/WanikaniCacheService.js
+++
@@ -1,6 +0,0 @@
-export default {
-    saveWanikaniApiKey: (apiKey) => localStorage.setItem('wanikani-api-key', apiKey),
-    loadWanikaniApiKey: () => localStorage.getItem('wanikani-api-key'),
-    saveWanikaniUser: (user) => localStorage.setItem('wanikani-user', JSON.stringify(user)),
-    loadWanikaniUser: () => !!localStorage.getItem('wanikani-user') ? JSON.parse(localStorage.getItem('wanikani-user')) : null,
-};

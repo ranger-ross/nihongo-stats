b395eb015c91816bc8962910ce5fd1c04a4cc85d
Ross Sullivan <sullivan.ross@heb.com>
2021-12-07 23:20:42 -0600

    Added level times to history summary

--- src/wanikani/charts/WanikaniHistorySummaryChart.jsx
+++ src/wanikani/charts/WanikaniHistorySummaryChart.jsx
@@ -27,6 +27,8 @@ async function fetchData() {
 }
 
 function numberWithCommas(x) {
+    if (isNaN(x) || x === '')
+        return '';
     return parseFloat(x).toLocaleString();
 }
 
@@ -43,9 +45,43 @@ function TotalLabel({ label, count, color }) {
     );
 }
 
+function DaysAndHoursLabel({ label, milliseconds }) {
+    const days = millisecondsToDays(milliseconds);
+    const hours = millisecondsToHours(milliseconds - (days * 1000 * 3600 * 24));
+    return (
+        <>
+            <Grid item xs={6}>{label}: </Grid>
+            <Grid item xs={6}>
+                <strong style={{ textShadow: '1px 1px 3px #000000aa' }}>
+                    {days} Days {hours} Hours
+                </strong>
+            </Grid>
+        </>
+    );
+}
+
+function getMedian(a) {
+    const n = a.length;
+    a = a.sort((a, b) => a - b);
+    if (n % 2 != 0)
+        return a[n / 2];
+    const bottomIndex = (n / 2) - 1;
+    const topIndex = n / 2;
+    return (a[bottomIndex] + a[topIndex]) / 2;
+}
+
+function millisecondsToDays(ms) {
+    return Math.floor(ms / (1000 * 60 * 60 * 24));
+}
+
+function millisecondsToHours(ms) {
+    return Math.floor(ms / (1000 * 60 * 60));
+}
+
 function WanikaniHistorySummaryChart() {
     const [data, setData] = useState([]);
     const [totalsData, setTotalsData] = useState({ total: 0, radicals: 0, kanji: 0, vocabulary: 0 });
+    const [levelData, setLevelData] = useState({ timeSinceStart: '', average: '', median: '' });
 
     useEffect(() => {
         let isSubscribed = true;
@@ -64,6 +100,44 @@ function WanikaniHistorySummaryChart() {
                 setData(data);
             })
             .catch(console.error);
+
+        WanikaniApiService.getUser()
+            .then(user => {
+                if (!isSubscribed)
+                    return;
+                const timeSinceStart = Date.now() - new Date(user.data['started_at']).getTime();
+                setLevelData(lvlData => ({
+                    ...lvlData,
+                    timeSinceStart
+                }));
+            });
+
+        WanikaniApiService.getLevelProgress()
+            .then(levelProgressData => {
+                if (!isSubscribed)
+                    return;
+
+                let levelTimes = [];
+                for (const level of levelProgressData.data) {
+                    const start = new Date(level.data['unlocked_at']).getTime();
+                    const end = !!level.data['passed_at'] ? new Date(level.data['passed_at']).getTime() : null;
+                    if (!end) {
+                        continue;
+                    }
+                    levelTimes.push(end - start);
+                }
+
+                const averageInMillis = levelTimes.reduce((sum, value) => sum + value) / levelTimes.length;
+                const medianInMillis = getMedian(levelTimes);
+
+                setLevelData(lvlData => ({
+                    ...lvlData,
+                    average: averageInMillis,
+                    median: medianInMillis
+                }));
+
+            });
+
         return () => isSubscribed = false;
     }, []);
 
@@ -74,12 +148,18 @@ function WanikaniHistorySummaryChart() {
                     History Summary
                 </Typography>
 
-                <div style={{ display: 'flex' }}>
-                    <Grid container style={{ maxWidth: '280px' }}>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                    <Grid container style={{ maxWidth: '280px', marginTop: '10px' }}>
                         <TotalLabel label={'Total Reviews'} count={totalsData.total} />
                         <TotalLabel label={'Radicals Reviews'} count={totalsData.radicals} color={wanikaniColors.blue} />
                         <TotalLabel label={'Kanji Reviews'} count={totalsData.kanji} color={wanikaniColors.pink} />
                         <TotalLabel label={'Vocabulary Reviews'} count={totalsData.vocabulary} color={wanikaniColors.purple} />
+                    </Grid>
+
+                    <Grid container style={{ maxWidth: '300px', marginTop: '10px' }}>
+                        <DaysAndHoursLabel label={'Time since start'} milliseconds={levelData.timeSinceStart} />
+                        <DaysAndHoursLabel label={'Average Time per level'} milliseconds={levelData.average} />
+                        <DaysAndHoursLabel label={'Median Time per level'} milliseconds={levelData.median} />
                     </Grid>
 
                 </div>

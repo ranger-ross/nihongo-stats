fae7e4f09be9e69fc44e264f8088d77ede2fad65
Ross Sullivan <sullivan.ross@heb.com>
2021-12-29 18:55:40 -0600

    Added Bunpro api doc urls

--- src/bunpro/service/BunProApiService.js
+++ src/bunpro/service/BunProApiService.js
@@ -1,3 +1,10 @@
+// API Notes
+// https://github.com/bunpro-srs/BunPro-iOS/blob/main/BunProKit/Sources/BunProKit/Server.swift#L16
+
+// All Reviews
+// https://bunpro.jp/api/v3/reviews/all_reviews_total
+// Header Authorization: "Token token=<api-key>"
+
 export default {
     getUser: function (token) {
         return fetch('/api/user/' + token);

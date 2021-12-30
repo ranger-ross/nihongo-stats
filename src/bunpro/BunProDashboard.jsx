0672c3ef494cb7fa33c0828584a386cc94b8910f
Ross Sullivan <sullivan.ross@heb.com>
2021-12-30 15:40:58 -0600

    Created JLPT progress tile

--- src/bunpro/BunProDashboard.jsx
+++ src/bunpro/BunProDashboard.jsx
@@ -3,6 +3,13 @@ import {Navigate} from "react-router";
 import {RoutePaths} from "../Routes.jsx";
 import {useBunProApiKey} from "../hooks/useBunProApiKey.jsx";
 import {BunProWelcomeTile} from "./components/BunProWelcomeTile.jsx";
+import BunProApiService from "./service/BunProApiService.js";
+import BunProPreloadedData from "./components/BunProPreloadedData.jsx";
+import {BunProJLPTTile} from "./components/BunProJLPTTile";
+import WanikaniWelcomeTile from "../wanikani/components/WanikaniWelcomeTile.jsx";
+import WanikaniLevelSummaryChart from "../wanikani/components/WanikaniLevelSummaryChart.jsx";
+import WanikaniItemCountsChart from "../wanikani/components/WanikaniItemCountsChart.jsx";
+import WanikaniFutureReviewsChart from "../wanikani/components/WanikaniFutureReviewsChart.jsx";
 
 const styles = {
     container: {
@@ -18,20 +25,35 @@ function BunProDashboard() {
     const {apiKey} = useBunProApiKey();
 
     useEffect(() => {
-        // BunProApiService.login(apiKey)
-        //     .then(console.log)
-        //     .catch(console.error);
+        BunProApiService.getUserProgress()
+            .then(console.log)
+            .catch(console.error);
     }, []);
 
     return (
+
         <div style={styles.container}>
             {!apiKey ? (<Navigate to={RoutePaths.bunproLogin} replace={true}/>) : (
-                <div style={styles.container}>
+                <BunProPreloadedData>
+                    <div style={styles.container}>
 
-                    <BunProWelcomeTile/>
+                        <div style={{display: 'flex', gap: '10px', flexWrap: 'wrap', alignItems: 'stretch'}}>
+
+                            <div style={{display: 'flex', gap: '10px', flexDirection: 'column', flexGrow: '1', minWidth: '500px'}}>
+                                <BunProWelcomeTile/>
+
+                                <BunProJLPTTile/>
+                            </div>
+
+                            <div style={{flexGrow: '25'}}>
+                                placeholder
+                            </div>
+
+                        </div>
 
 
-                </div>
+                    </div>
+                </BunProPreloadedData>
             )}
 
         </div>
---
+++ src/bunpro/components/BunProPreloadedData.jsx
@@ -0,0 +1,90 @@
+import {useEffect, useState} from "react";
+import {CircularProgress} from "@mui/material";
+import CheckIcon from '@mui/icons-material/Check';
+import {useWanikaniPreloadStatus} from "../../hooks/useWanikaniPreloadStatus.jsx";
+import BunProApiService from "../service/BunProApiService.js";
+
+const styles = {
+    loadingItem: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'left',
+        gap: '10px'
+    },
+    loadingItemsContainer: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '20%'
+    },
+    loadingItemsColumn: {
+        display: 'flex',
+        flexDirection: 'column'
+    }
+};
+
+
+function LoadingItem({text, isLoading}) {
+    return (
+        <div style={styles.loadingItem}>
+            {text}
+            {isLoading ? <CircularProgress size={15}/> : <CheckIcon style={{color: 'lime'}}/>}
+        </div>
+    );
+}
+
+function BunProPreloadedData({children}) {
+    const [userProgress, setUserProgress] = useState(false);
+    const [grammarPoints, setGrammarPoints] = useState(false);
+    const [allReviews, setAllReviews] = useState(false);
+    const {status, setStatus} = useWanikaniPreloadStatus();
+
+    useEffect(() => {
+        if (status) {
+            return;
+        }
+        console.log('Preloading BunPro Data');
+
+        Promise.all([
+            BunProApiService.getUserProgress()
+                .then(() => setUserProgress(true)),
+            BunProApiService.getGrammarPoints()
+                .then(() => setGrammarPoints(true)),
+            BunProApiService.getAllReviews()
+                .then(() => setAllReviews(true)),
+        ])
+            .then(() => {
+                console.log('BunPro Data preloaded');
+                setStatus(true);
+            });
+    }, []);
+
+    const isLoaded = status || (userProgress && grammarPoints && allReviews);
+
+    return (
+        <>
+            {isLoaded ? (
+                children
+            ) : (
+                <>
+                    <div style={styles.loadingItemsContainer}>
+                        <div style={styles.loadingItemsColumn}>
+                            <strong>Loading BunPro Data...</strong>
+                            <br/>
+                            <LoadingItem text={'Grammar Points'} isLoading={!grammarPoints}/>
+                            <LoadingItem text={'User Progress'} isLoading={!userProgress}/>
+                            <LoadingItem text={'Reviews'} isLoading={!grammarPoints}/>
+                        </div>
+                    </div>
+
+                    <p style={{textAlign: 'center'}}>
+                        This may take a few minutes.
+                    </p>
+
+                </>
+            )}
+        </>
+    );
+}
+
+export default BunProPreloadedData;

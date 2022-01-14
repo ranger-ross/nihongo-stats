16b438d85af0e5778392a2e39a0153c3bddc922e
Ross Sullivan <sullivan.ross@heb.com>
2022-01-14 13:45:40 -0600

    #49 Added Total Lessons and Burned Items data in Wanikani history summary

---
+++ src/util/test/ArrayUtils.test.js
@@ -0,0 +1,23 @@
+import {distinct} from "../ArrayUtils.js";
+
+describe('ArrayUtils', function () {
+
+    describe('distinct()', function () {
+
+        it('should return [1, 2] for input [1, 2, 2, 1]', function () {
+            const result = distinct([1, 2, 2, 1]);
+            expect(result.length).toBe(2);
+            expect(result[0]).toBe(1);
+            expect(result[1]).toBe(2);
+        });
+
+        it('should return [{x:1}, {x:2}}] for input [{x:1}, {x:2}, {x:2}, {x:1}], e => e.x', function () {
+            const result = distinct([{x: 1}, {x: 2}, {x: 2}, {x: 1}], e => e.x);
+            expect(result.length).toBe(2);
+            expect(result[0].x).toBe(1);
+            expect(result[1].x).toBe(2);
+        });
+
+    });
+
+});

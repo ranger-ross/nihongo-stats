a6cd09c46f378439808da39c304df294233af133
Ross Sullivan <sullivan.ross@heb.com>
2022-01-14 17:17:43 -0600

    Fixed compare function bug

--- src/util/ArrayUtils.js
+++ src/util/ArrayUtils.js
@@ -1,10 +1,12 @@
 export const distinct = function (array, compareFn) {
+    const compare = !!compareFn ? compareFn : entry => entry;
+
     let flags = {};
     return array.filter(entry => {
-        if (flags[compareFn(entry)]) {
+        if (flags[compare(entry)]) {
             return false;
         }
-        flags[compareFn(entry)] = true;
+        flags[compare(entry)] = true;
         return true;
     });
 };

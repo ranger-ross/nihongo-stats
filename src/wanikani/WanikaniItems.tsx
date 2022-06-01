adbae85d988ef39e30eee98b96fce6d11d142680
Ross Sullivan <sullivan.ross@heb.com>
2022-05-31 22:22:40 -0500

    Convert wk items to typescript

--- src/wanikani/WanikaniItems.tsx
+++ src/wanikani/WanikaniItems.tsx
@@ -1,16 +1,26 @@
-import {useWanikaniApiKey} from "../hooks/useWanikaniApiKey.tsx";
+import {useWanikaniApiKey} from "../hooks/useWanikaniApiKey";
+// @ts-ignore
 import {RoutePaths} from "../Routes";
 import * as React from "react";
 import {useEffect, useMemo, useState} from "react";
-import RequireOrRedirect from "../shared/RequireOrRedirect.tsx";
+import RequireOrRedirect from "../shared/RequireOrRedirect";
 import {Card, CardContent, Typography} from "@mui/material";
-import WanikaniItemTile from "./components/WanikaniItemTile.tsx";
-import WanikaniApiService from "./service/WanikaniApiService.ts";
-import {combineAssignmentAndSubject, createAssignmentMap, isSubjectHidden} from "./service/WanikaniDataUtil.ts";
-import WanikaniItemsControlPanel, {useWanikaniItemControls} from "./components/WanikaniItemsControlPanel.tsx";
+import WanikaniItemTile from "./components/WanikaniItemTile";
+import WanikaniApiService from "./service/WanikaniApiService";
+import {
+    combineAssignmentAndSubject,
+    createAssignmentMap,
+    isSubjectHidden,
+    JoinedRawWKAssignmentAndSubject,
+    WKColorByOption,
+    WKGroupByOption,
+    WKSortByOption
+} from "./service/WanikaniDataUtil";
+import WanikaniItemsControlPanel, {useWanikaniItemControls} from "./components/WanikaniItemsControlPanel";
 import VisibilitySensor from "react-visibility-sensor";
+import {AppStyles} from "../util/TypeUtils";
 
-const styles = {
+const styles: AppStyles = {
     container: {
         margin: '5px'
     },
@@ -22,14 +32,19 @@ const styles = {
     },
 };
 
-function SubjectTile({subject, colorBy}) {
+type SubjectTileProps = {
+    subject: JoinedRawWKAssignmentAndSubject,
+    colorBy: WKColorByOption
+};
+
+function SubjectTile({subject, colorBy}: SubjectTileProps) {
     return useMemo(() => (
         <WanikaniItemTile
             text={subject.characters || '?'}
             link={subject['document_url']}
             meaning={subject?.meanings?.map(m => m.meaning).join(', ')}
             srsLevel={subject['srs_stage']}
-            color={colorBy.color(subject)}
+            color={colorBy.color(subject) as string}
             size={5}
             type={subject.subjectType}
             level={subject.level}
@@ -39,7 +54,15 @@ function SubjectTile({subject, colorBy}) {
     ), [subject, colorBy.key]);
 }
 
-function ItemGroupingData({subjects, secondaryGroupBy, sortBy, colorBy, sortReverse}) {
+type ItemGroupingDataProps = {
+    subjects: JoinedRawWKAssignmentAndSubject[],
+    secondaryGroupBy: WKGroupByOption,
+    sortBy: WKSortByOption,
+    colorBy: WKColorByOption,
+    sortReverse: boolean
+};
+
+function ItemGroupingData({subjects, secondaryGroupBy, sortBy, colorBy, sortReverse}: ItemGroupingDataProps) {
     const subGroups = useMemo(() => secondaryGroupBy.group(subjects), [subjects, secondaryGroupBy.key]);
 
     const sortedSubGroups = useMemo(() => subGroups.map(sg => ({
@@ -74,7 +97,17 @@ function UnloadedGrouping() {
     );
 }
 
-function ItemGrouping({title, subjects, secondaryGroupBy, sortBy, colorBy, sortReverse}) {
+
+type ItemGroupingProps = {
+    title: string,
+    subjects: JoinedRawWKAssignmentAndSubject[],
+    secondaryGroupBy: WKGroupByOption,
+    sortBy: WKSortByOption,
+    colorBy: WKColorByOption,
+    sortReverse: boolean
+};
+
+function ItemGrouping({title, subjects, secondaryGroupBy, sortBy, colorBy, sortReverse}: ItemGroupingProps) {
     const [isLoaded, setIsLoaded] = useState(false);
     return (
         <Card style={{margin: '5px'}}>
@@ -90,7 +123,7 @@ function ItemGrouping({title, subjects, secondaryGroupBy, sortBy, colorBy, sortR
                 </div>
                 <VisibilitySensor partialVisibility={true}
                                   offset={{bottom: -300}}
-                                  onChange={(isVisible) => isVisible ? setIsLoaded(true) : null}>
+                                  onChange={(isVisible: boolean) => isVisible ? setIsLoaded(true) : null}>
                     {isLoaded ? (
                         <ItemGroupingData subjects={subjects}
                                           secondaryGroupBy={secondaryGroupBy}
@@ -108,34 +141,46 @@ function ItemGrouping({title, subjects, secondaryGroupBy, sortBy, colorBy, sortR
 
 async function fetchItems() {
     const subjects = await WanikaniApiService.getSubjects();
-    let assignments = await WanikaniApiService.getAllAssignments();
-
-    assignments = createAssignmentMap(assignments);
+    const assignments = await WanikaniApiService.getAllAssignments();
+    const assignmentMap = createAssignmentMap(assignments);
 
     return subjects
         .filter(subject => !isSubjectHidden(subject))
-        .map(s => combineAssignmentAndSubject(assignments[s.id], s));
+        .map(s => combineAssignmentAndSubject(assignmentMap[s.id], s));
 }
 
-function filterSubjectsByType(subjects, typesToShow) {
-    let lookupMap = {};
+function filterSubjectsByType(subjects: JoinedRawWKAssignmentAndSubject[], typesToShow: string[]) {
+    const lookupMap: { [key: string]: boolean } = {};
 
-    for (let type of typesToShow) {
+    for (const type of typesToShow) {
         lookupMap[type.toLowerCase()] = true;
     }
     return subjects.filter(subject => lookupMap[subject.subjectType.toLowerCase()]);
 }
 
+function useSubjects() {
+    const [subjects, setSubjects] = useState<JoinedRawWKAssignmentAndSubject[]>([]);
+
+    useEffect(() => {
+        let isSubscribed = true;
+        fetchItems()
+            .then(data => {
+                if (!isSubscribed)
+                    return;
+                setSubjects(data);
+            });
+        return () => {
+            isSubscribed = false;
+        }
+    }, []);
+
+    return subjects;
+}
 
 function WanikaniItems() {
     const {apiKey} = useWanikaniApiKey();
-    const [subjects, setSubjects] = useState([]);
+    const subjects = useSubjects();
     const {control, set} = useWanikaniItemControls();
-
-    useEffect(() => {
-        fetchItems().then(setSubjects)
-    }, []);
-
 
     const subjectsToShow = useMemo(() => filterSubjectsByType(subjects, control.typesToShow), [subjects, control.typesToShow]);
 

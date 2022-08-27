fa29a7389437ae8bf0b6bc1b4de6b2286b904183
Ross <rosssullivan101@gmail.com>
2022-08-27 16:09:38 +0900

    Upgraded to React 18 (#185)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import App from './App'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const container = document.getElementById('root');
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(container!);
+root.render(<App />);

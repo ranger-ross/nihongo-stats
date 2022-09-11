08cbf77d34dd7a85edd209b9792b221c5555fb84
ranger-ross <rosssullivan101@gmail.com>
2022-09-11 15:27:25 +0900

    Fixed linting

--- src/shared/SimpleCard.tsx
+++ src/shared/SimpleCard.tsx
@@ -9,7 +9,7 @@ type SimpleCardProps = {
 /**
  * A simple wrapper for the MUI Card.
  */
-export const SimpleCard = ({children, title}: SimpleCardProps) => {
+export function SimpleCard({children, title}: SimpleCardProps) {
     return (
         <Card title={title}>
             <CardContent>

5e107e8dac1c1ea54a430efda41468d1ba14863b
ranger-ross <rosssullivan101@gmail.com>
2022-09-15 23:10:26 +0900

    Clean up

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,6 +32,7 @@ module.exports = {
         'react/prop-types': 'off',
         'react/no-unescaped-entities': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-inferrable-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off', // TODO: ReEnable when project is cleaned up and better migrated to TypeScript
     },
     'settings': {

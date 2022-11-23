2ea8f6f753b393eefe0457a301b27c7919eac876
ranger-ross <rosssullivan101@gmail.com>
2022-11-23 14:03:52 +0900

    Do not setup sentry for local-dev

--- src/main.tsx
+++ src/main.tsx
@@ -6,27 +6,29 @@ import * as Sentry from "@sentry/react";
 import {useAppVersion} from "./hooks/useAppVersion";
 
 try {
-    Sentry.init({
-        dsn: "https://15b4201ec2eb4677be9f58c25f66ff05@o4504206450556928.ingest.sentry.io/4504206451998720",
-        integrations: [new BrowserTracing()],
+    if (useAppVersion() !== 'local-dev') {
+        Sentry.init({
+            dsn: "https://15b4201ec2eb4677be9f58c25f66ff05@o4504206450556928.ingest.sentry.io/4504206451998720",
+            integrations: [new BrowserTracing()],
 
-        // Set tracesSampleRate to 1.0 to capture 100%
-        // of transactions for performance monitoring.
-        // We recommend adjusting this value in production
-        tracesSampleRate: 1.0,
-        release: useAppVersion(),
-        environment: useAppVersion() == 'local-dev' ? 'local-dev' : 'production',
-        beforeSend: (event, hint) => {
-            // Ignore Anki Connect errors.
-            // Since we are polling, if Anki is not open there are lots of errors in the background
-            if (event.request?.url?.includes('anki') ||
-                event.request?.url?.includes('localhost:8765')) {
-                console.debug('not sending anki error')
-                return null;
+            // Set tracesSampleRate to 1.0 to capture 100%
+            // of transactions for performance monitoring.
+            // We recommend adjusting this value in production
+            tracesSampleRate: 1.0,
+            release: useAppVersion(),
+            environment: 'production',
+            beforeSend: (event, hint) => {
+                // Ignore Anki Connect errors.
+                // Since we are polling, if Anki is not open there are lots of errors in the background
+                if (event.request?.url?.includes('anki') ||
+                    event.request?.url?.includes('localhost:8765')) {
+                    console.debug('not sending anki error')
+                    return null;
+                }
+                return event
             }
-            return event
-        }
-    });
+        });
+    }
 } catch (error) {
     console.error('failed to setup sentry', error);
 }

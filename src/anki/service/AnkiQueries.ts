7663cf858ab9e75572556c55d78d2a9bfd720c85
ranger-ross <rosssullivan101@gmail.com>
2024-04-27 19:32:01 +0900

    migrated to tanstack query v5

--- src/anki/service/AnkiQueries.ts
+++ src/anki/service/AnkiQueries.ts
@@ -18,43 +18,53 @@ const ANKI_STALE_TIME = 10 * 1000;
 
 
 export function useAnkiDeckSummaries(decks: string[]) {
-    return useQuery<AnkiDeckSummary[]>([ANKI_QUERY_KEY, 'DeckSummaries', ...decks], () => fetchAnkiDeckSummaries(decks), {
+    return useQuery<AnkiDeckSummary[]>({
+        queryKey: [ANKI_QUERY_KEY, 'DeckSummaries', ...decks], 
+        queryFn: () => fetchAnkiDeckSummaries(decks), 
         enabled: !!decks && decks.length > 0,
-        cacheTime: Infinity,
+        gcTime: Infinity,
         staleTime: ANKI_STALE_TIME,
     });
 }
 
 export function useAnkiCardBreakdown(decks: string[]) {
-    return useQuery([ANKI_QUERY_KEY, 'CardBreakDown', ...decks], () => fetchCardBreakDownData(decks), {
+    return useQuery({
+        queryKey:  [ANKI_QUERY_KEY, 'CardBreakDown', ...decks],
+        queryFn:  () => fetchCardBreakDownData(decks), 
         enabled: !!decks && decks.length > 0,
-        cacheTime: Infinity,
+        gcTime: Infinity,
         staleTime: ANKI_STALE_TIME,
     });
 }
 
 export function useAnkiUpcomingReviews(decks: string[], days: number) {
-    return useQuery([ANKI_QUERY_KEY, 'UpcomingReviews', ...decks, days], () => fetchAnkiUpcomingReviewData(decks, days), {
+    return useQuery({
+        queryKey:  [ANKI_QUERY_KEY, 'UpcomingReviews', ...decks, days], 
+        queryFn: () => fetchAnkiUpcomingReviewData(decks, days), 
         enabled: !!decks && decks.length > 0,
-        cacheTime: Infinity,
+        gcTime: Infinity,
         staleTime: ANKI_STALE_TIME,
         select: data => mapToUpcomingAnkiReviewDataPoints(decks, data)
     });
 }
 
 export function useAnkiReviewsByDeck(decks: string[]) {
-    return useQuery([ANKI_QUERY_KEY, 'AnkiReviewsByDeck', ...decks], () => fetchAnkiReviewsByDeck(decks), {
+    return useQuery({
+        queryKey:   [ANKI_QUERY_KEY, 'AnkiReviewsByDeck', ...decks], 
+        queryFn: () => fetchAnkiReviewsByDeck(decks), 
         enabled: !!decks && decks.length > 0,
-        cacheTime: Infinity,
+        gcTime: Infinity,
         staleTime: ANKI_STALE_TIME,
         select: data => mapToDeckReviews(decks, data)
     });
 }
 
 export function useAnkiBreakDownHistory(decks: string[]) {
-    return useQuery([ANKI_QUERY_KEY, 'BreakDownHistory', ...decks], () => fetchBreakDownHistoryData(decks), {
+    return useQuery({
+        queryKey:   [ANKI_QUERY_KEY, 'BreakDownHistory', ...decks], 
+        queryFn: () => fetchBreakDownHistoryData(decks), 
         enabled: !!decks && decks.length > 0,
-        cacheTime: Infinity,
+        gcTime: Infinity,
         staleTime: ANKI_STALE_TIME,
     });
 }

c642b5c901d2a938e16d02057fc57f3bfabdafb6
Ross <rosssullivan101@gmail.com>
2024-07-20 13:22:05 +0900

    Update to Sentry 8 (#685)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import App from './App'
 import {createRoot} from 'react-dom/client';
-import {BrowserTracing} from "@sentry/tracing";
 import * as Sentry from "@sentry/react";
 import {useAppVersion} from "./hooks/useAppVersion";
 
@@ -9,7 +8,7 @@ try {
     if (useAppVersion() !== 'local-dev') {
         Sentry.init({
             dsn: "https://15b4201ec2eb4677be9f58c25f66ff05@o4504206450556928.ingest.sentry.io/4504206451998720",
-            integrations: [new BrowserTracing()],
+            integrations: [Sentry.browserTracingIntegration()],
 
             // Set tracesSampleRate to 1.0 to capture 100%
             // of transactions for performance monitoring.
